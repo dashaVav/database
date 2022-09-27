efd880ab5f55112159bcf230e89a138ef3b6425f
Dasha <dashavavilova2002@icloud.com>
2022-09-27 14:04:39 +0300

    на 9, работа с несколькими базами

--- main.py
+++ main.py
@@ -6,61 +6,73 @@ import os.path
 class Database:
     def __init__(self, folder):
         self.student_id = 1
-        # os.mkdir(folder)
-        # f = open(folder + '\\' + 'k', 'a+')
         if not os.path.exists(folder):
             os.mkdir(folder)
+        self.student_txt = folder + '\\' + 'students.txt'
+        self.variants_txt = folder + '\\' + 'variants.txt'
+        self.testing_table_txt = folder + '\\' + 'testing_table.txt'
+        self.copy = folder + '\\' + 'copy.txt'
+        self.update()
+        self.add_from()
+        self.variants()
 
     # past id from students.txt
     def update(self):
-        if not os.path.exists('students.txt'):
+        if not os.path.exists(self.student_txt):
             return 0
-        with open('students.txt') as f:
+        with open(self.student_txt) as f:
             s = f.readlines()
-        if len(s) == 0:
-            return 0
         self.student_id = int(s[len(s) - 1].split(' ', 1)[0]) + 1
 
-    # add new id_students in testing_table
+    # add  id_students in testing_table
     def add_testing_table(self):
-        file = open('testing_table.txt', 'a+')
+        file = open(self.testing_table_txt, 'a+')
         file.write(str(self.student_id - 1) + ' ' + str(random.randint(1, 99)) + '\n')
         file.close()
 
-    def add(self, name, surname, patronymic):
-        with open('students.txt', 'r') as f:
+    def duplicate(self, name, surname, patronymic):
+        if not os.path.exists(self.student_txt): return 1
+        with open(self.student_txt, 'r') as f:
             s = f.readlines()
         for i in s:
             i = i.replace('\n', '')
             id_st, name_, surname_, patronymic_ = i.split(' ')
             if name_ == name and surname_ == surname and patronymic_ == patronymic:
                 return 0
-        file = open('students.txt', 'a+')
-        file.write(' '.join([str(self.student_id), name, surname, patronymic]) + '\n')
-        self.student_id += 1
-        file.close()
-        self.add_testing_table()
+        return 1
+
+    # no people with the same name
+    def add(self, name, surname, patronymic):
+        if self.duplicate(name, surname, patronymic) != 0:
+            file = open(self.student_txt, 'a+')
+            file.write(' '.join([str(self.student_id), name, surname, patronymic]) + '\n')
+            self.student_id += 1
+            file.close()
+            self.add_testing_table()
+        else:
+            print('cannot be added')
 
     # add new students from names.txt
+    # auto when creating
     def add_from(self):
-        if os.path.exists('students.txt'):
+        if not os.path.exists('names.txt'): return 0
+        if os.path.exists(self.student_txt):
             return 0
-
-        file_1 = open('students.txt', "a+")
-        file_1.close()
-        file = open("names.txt", encoding="utf8")
+        file = open(self.student_txt, "a+")
+        file.close()
+        file_names = open("names.txt", encoding="utf8")
         while 1:
-            s = file.readline()
+            s = file_names.readline()
             if not s:
                 break
             s = s.replace('\n', '')
             first_name, second_name, patronymic = s.split(' ')
             self.add(first_name, second_name, patronymic)
-        file.close()
+        file_names.close()
 
     # print student's name by id
     def print_id(self, student_id):
-        file = open('students.txt', 'r')
+        file = open(self.student_txt, 'r')
         while 1:
             s = file.readline()
             if not s: break
@@ -69,9 +81,20 @@ class Database:
                 break
         file.close()
 
+    def print_if_via_name(self, name, surname, patronymic):
+        with open(self.student_txt, 'r') as f:
+            s = f.readlines()
+        for i in s:
+            i = i.replace('\n', '')
+            id_st, name_, surname_, patronymic_ = i.split(' ')
+            if name_ == name and surname_ == surname and patronymic_ == patronymic:
+                print(id_st)
+                return 0
+        print('not found')
+
     def remove_testing_table(self, student_id):
-        file = open('testing_table.txt', 'r+')
-        file_new = open('new_testing_table.txt', 'a+')
+        file = open(self.testing_table_txt, 'r+')
+        file_new = open(self.copy, 'a+')
         while 1:
             s = file.readline()
             if not s:
@@ -81,13 +104,12 @@ class Database:
                 file_new.write(s)
         file_new.close()
         file.close()
-
-        os.remove('testing_table.txt')
-        os.rename('new_testing_table.txt', 'testing_table.txt')
+        os.remove(self.testing_table_txt)
+        os.rename(self.copy, self.testing_table_txt)
 
     def remove(self, student_id):
-        file = open('students.txt', 'r+')
-        file_new = open('new_students.txt', 'a+')
+        file = open(self.student_txt, 'r+')
+        file_new = open(self.copy, 'a+')
         while 1:
             s = file.readline()
             if not s:
@@ -97,114 +119,96 @@ class Database:
                 file_new.write(s)
         file_new.close()
         file.close()
-
-        os.remove('students.txt')
-        os.rename('new_students.txt', 'students.txt')
+        os.remove(self.student_txt)
+        os.rename(self.copy, self.student_txt)
         self.remove_testing_table(student_id)
 
     # 99 id - 9 var
     def variants(self):
-        if os.path.exists('variants.txt'):
+        if os.path.exists(self.variants_txt):
             return 0
-        file = open('variants.txt', 'a+')
+        file = open(self.variants_txt, 'a+')
         var = [i//10 for i in range(100)]
         random.shuffle(var)
         for i in range(1, 100):
             file.write(str(i) + ' ' + 'var' + str(var[i]) + '\n')
         file.close()
 
+    # edit students with id
     def edit(self, student_id, name, surname, patronymic):
         f = 0
-        with open('students.txt', 'r') as f:
+        with open(self.student_txt, 'r') as f:
             s = f.readlines()
         for i in s:
             i = i.replace('\n', '')
             id_st, name_, surname_, patronymic_ = i.split(' ')
             if student_id == id_st: f = 1
             if name_ == name and surname_ == surname and patronymic_ == patronymic:
-                print('no')
+                print('cannot edit')
                 return 0
-        if f:
-            file = open('students.txt', 'r+')
-            file_new = open('new_students.txt', 'a+')
-            while 1:
-                s = file.readline()
-                if not s:
-                    print('not found')
-                    break
-                if int(s.split(' ', 1)[0]) != student_id:
-                    file_new.write(s)
-                else:
-                    file_new.write(' '.join([str(s.split(' ', 1)[0]), name, surname, patronymic]) + '\n')
+        if f == 0:
+            print('not found')
+            return 0
 
-            file_new.close()
-            file.close()
-
-            os.remove('students.txt')
-            os.rename('new_students.txt', 'students.txt')
-
-    def print_table(self):
-        file_testing_table = open('testing_table.txt', 'r')
+        file = open(self.student_txt, 'r+')
+        file_new = open(self.copy, 'a+')
         while 1:
-            s = file_testing_table.readline()
-            # print(s)
+            s = file.readline()
             if not s:
                 break
-            s = s.replace('\n','')
+            if int(s.split(' ', 1)[0]) != student_id:
+                file_new.write(s)
+            else:
+                file_new.write(' '.join([str(s.split(' ', 1)[0]), name, surname, patronymic]) + '\n')
+        file_new.close()
+        file.close()
+        os.remove(self.student_txt)
+        os.rename(self.copy, self.student_txt)
+
+    def print_table(self):
+        file_testing_table = open(self.testing_table_txt, 'r')
+        while 1:
+            s = file_testing_table.readline()
+            if not s:
+                break
+            s = s.replace('\n', '')
             student_id, student_var = s.split(' ')
-            file = open('students.txt', 'r')
+
+            file = open(self.student_txt, 'r')
             while 1:
-                str = file.readline()
-                str = str.replace('\n', '')
-                name = str.split(' ', 1)
-                # print(name)
+                str_ = file.readline()
+                str_ = str_.replace('\n', '')
+                name = str_.split(' ', 1)
                 if student_id == name[0]:
                     break
             file.close()
-            file = open('variants.txt', 'r')
+
+            file = open(self.variants_txt, 'r')
             while 1:
                 str_ = file.readline()
                 str_ = str_.replace('\n', '')
                 var = str_.split(' ')
-                # print(student_var, var[0])
                 if student_var == var[0]:
                     break
             file.close()
-
             print(*(name[1:3]), var[1])
-
         file_testing_table.close()
 
 
-base = Database('hse_students')
-base.update()
-base.add_from()
+base = Database('hse_students_math')
+base.print_if_via_name('Вавилова', 'Дарья', 'Григорьевна')
 
-base.add_from()
+
+
 base.variants()
-base.print_id(5)
-base.add('kk', 'll', 'll')
-base.add('kk', 'll', 'll1')
+# base.print_id(5)
+# base.add('kk', 'll', 'll')
+base.add('kk8989', 'll', 'll1')
 # base.testing_table()
-base.remove(8)
-base.edit(12, 'Борисоjjjва', 'Елизаветnа',  'Юрьевнkkа')
+# base.remove(8)
+# base.edit(12, 'Борисоjjjва', 'Елизаветnа',  'Юрьевнkkа')
 base.print_table()
-# base.testing_table()
-# var = [i//10 for i in range(100)]
-# print(len(var))
-# base.remove('students.txt', 1)
-# base.remove('students.txt', 77)
-# решить как айди таскать
-#
-# file = open("names.txt", encoding="utf8")
-# s = file.readline()
-# s = s.replace("\n", "")
-# first_name, second_name, patronymic = s.split(" ")
-# print(s)
-# print(first_name + "+" + second_name + "=" + patronymic+"=")
-# s = file.readline()
-# print(s)
-#
-# file_in = open("data.txt", 'w+')
-# file_in.write("1"+" "+first_name + "+" + second_name + "=" + patronymic+"=")
+# base.print_id(89)
 
+
+base = Database('hse_students_lit')
